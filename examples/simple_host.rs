a303c77818aacce23f8d500f2b09d1b052930803
Alec Deason <alec@tinycountry.com>
2021-12-01 17:11:37 -0800

    Simplify the basic example

--- examples/simple_host.rs
+++ examples/simple_host.rs
@@ -1,9 +1,12 @@
+use hound;
 use virtual_modular::InstrumentSynth;
 use virtual_modular_core_nodes::*;
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-
 fn main() {
+    let output_path = std::env::args()
+        .nth(1)
+        .expect("Must supply a path for the output WAV file");
+
     // Make a sine oscillator running at 220 hz
     let osc = Pipe(Branch(Constant(220.0), Constant(0.0)), Sine::default());
 
@@ -14,76 +17,32 @@ fn main() {
     let builder = InstrumentSynth::builder();
     let mut synth = builder.build_with_synth(sound);
 
-    // Below this is mostly just cpal boilerplate to play sound
-    // The pieces worth paying attention to are the calls to
-    // synth.set_sample_rate and synth.process
+    synth.set_sample_rate(44100.0);
 
-    let cpal_host = cpal::default_host();
+    // Render one second of sound to buffers
+    let mut left_buffer = vec![0.0; 44100];
+    let mut right_buffer = vec![0.0; 44100];
+    synth.process(&mut left_buffer, &mut right_buffer);
 
-    let device = cpal_host.default_output_device().unwrap();
-    let config = device.default_output_config().unwrap();
-
-    let sample_rate = config.sample_rate().0 as f32;
-    synth.set_sample_rate(sample_rate);
-    let _cpal_stream = match config.sample_format() {
-        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), synth),
-        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), synth),
-        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), synth),
+    // Write out a WAV file. The system is designed to be real time but
+    // for simplicity I'm using hound in this example rather than cpal
+    // or jack. For a complete realtime example look in the dynamic_environment directory
+    let spec = hound::WavSpec {
+        channels: 2,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
     };
 
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(10));
+    let mut writer = hound::WavWriter::create(output_path, spec).unwrap();
+    let amplitude = i16::MAX as f32;
+    for (l, r) in left_buffer.iter().zip(&right_buffer) {
+        writer
+            .write_sample((l.min(1.0) * amplitude) as i16)
+            .unwrap();
+        writer
+            .write_sample((r.min(1.0) * amplitude) as i16)
+            .unwrap();
     }
-}
-
-fn run<T>(
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-    mut synth: InstrumentSynth,
-) -> cpal::Stream
-where
-    T: cpal::Sample,
-{
-    let channels = config.channels as usize;
-
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-    let mut outputs = vec![vec![0.0; 128]; 2];
-
-    let stream = device
-        .build_output_stream(
-            config,
-            move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-                write_data(data, channels, &mut synth, &mut outputs)
-            },
-            err_fn,
-        )
-        .unwrap();
-    stream.play().unwrap();
-    stream
-}
-
-fn write_data<T>(
-    output: &mut [T],
-    channels: usize,
-    synth: &mut InstrumentSynth,
-    outputs: &mut Vec<Vec<f32>>,
-) where
-    T: cpal::Sample,
-{
-    outputs[0].resize(output.len() / 2, 0.0);
-    outputs[0].fill(0.0);
-    outputs[1].resize(output.len() / 2, 0.0);
-    outputs[1].fill(0.0);
-
-    let (left, tail) = outputs.split_at_mut(1);
-    synth.process(&mut left[0], &mut tail[0]);
-
-    for (i, frame) in output.chunks_mut(channels).enumerate() {
-        let value_left = outputs[0][i];
-        let value_right = outputs[1][i];
-
-        frame[0] = cpal::Sample::from::<f32>(&(value_left * 0.5));
-        frame[1] = cpal::Sample::from::<f32>(&(value_right * 0.5));
-    }
+    writer.finalize().unwrap();
 }
